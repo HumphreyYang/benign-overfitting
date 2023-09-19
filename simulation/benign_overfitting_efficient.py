588b3f6df0f2c8035e8817f282b2d315357b5a07
Humphrey Yang <humphrey.yang@anu.edu.au>
2023-09-20 09:29:06 +1000

    update orthonormal matrix

--- simulation/benign_overfitting_efficient.py
+++ simulation/benign_overfitting_efficient.py
@@ -9,6 +9,7 @@ import numba
 from numba import prange
 from numba.typed import List
 from numba_progress import ProgressBar
+import argparse
 
 @numba.njit(cache=True, fastmath=True, nogil=True)
 def solve_β_hat(X, Y):
@@ -53,13 +54,11 @@ def scale_norm(β, snr):
     return β_normalized
 
 @numba.njit(cache=True, fastmath=True, nogil=True)
-def generate_orthonormal_matrix(dim, seed=None):
+def generate_orthonormal_matrix(dim):
     """
     Generate random orthonormal matrix of size dim x dim.
     """
-        
-    np.random.seed(seed+1)
-    a = np.random.randn(dim, dim)
+    a = np.ones((dim, dim))
     res, _ = np.linalg.qr(a)
     return np.ascontiguousarray(res)
 
@@ -74,15 +73,15 @@ def compute_X(λ, μ, n, p, seed=None):
     C is a p x p matrix with eigenvalues (λ, 1, ..., 1).
     """
 
-    U = generate_orthonormal_matrix(p, seed=seed+1)
-    V = generate_orthonormal_matrix(n, seed=seed+2)
+    U = generate_orthonormal_matrix(p)
+    V = generate_orthonormal_matrix(n)
 
     Λ = np.diag(np.concatenate((np.array([λ]), np.ones(p-1))))
     C = (U @ Λ) @ U.T
     A = np.diag(np.concatenate((np.array([μ]), np.ones(n-1))))
     Γ = (V @ A) @ V.T
     
-    np.random.seed(seed+3)
+    np.random.seed(seed)
     Z = np.random.normal(0, 1, (n, p))
     return Γ @ (Z @ C)
 
@@ -95,27 +94,22 @@ def compute_ε(σ, n, seed=None):
     np.random.seed(seed)
     return np.random.normal(0, σ, n)
 
+@numba.njit(cache=True, fastmath=True, nogil=True)
 def simulate_risks(X, ε, params):
     """
     Fit the LS model and calculate the test MSE and null risk.
     """
     λ, μ, p, n, snr = params
     X_p = np.ascontiguousarray(X[:, :p])
-    β = scale_norm(np.random.normal(0, 1, p), snr)
-    print("X_p: ", X_p.shape)
-    print("β: ", β.shape)
-    print("ε: ", ε.shape)
+    β = scale_norm(np.ones(p), snr)
     Y = compute_Y(X_p, β, ε)
     null_risk = np.linalg.norm(β)**2
     print(null_risk)
-    X_train = X_p[:n,:]
+    X_train = X_p[:n, :]
     X_test = X_p[n:, :]
     Y_train = Y[:n]
     β_hat = solve_β_hat(X_train, Y_train)
     test_MSE = calculate_MSE(β_hat, β, X_test)
-    print('testMSE: ', test_MSE)
-    print(X_p.shape[0])
-    print('MSE_groud_truth: ', np.sum((Y - X_p @ β_hat)**2) / X_p.shape[0])
     return np.array([λ, μ, p, n, snr, test_MSE], 
                                 dtype=np.float64)
     
@@ -147,18 +141,38 @@ def generate_symlog_points(n1, n2, L, U, a):
     
     return symlog_points
 
-if __name__ == "__main__":
-    μ_array = np.array([1, 100, 200, 500])
-    # μ_array = np.array([1])
-    λ_array = np.array([1])
-    n1, n2 = 30, 30
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Run simulations with varying parameters.')
+    
+    parser.add_argument('--mu', type=int, nargs='+', default=[1, 100, 200, 500], help='Array of mu values.')
+    parser.add_argument('--Lambda', type=int, nargs='+', default=[1], help='Array of lambda values.')
+    parser.add_argument('--n1', type=int, default=30, help='Parameter n1 for symlog points before center point.')
+    parser.add_argument('--n2', type=int, default=30, help='Parameter n2 for symlog points after center point.')
+    parser.add_argument('--n_array', type=int, nargs='+', default=[200], help='Array of n values.')
+    parser.add_argument('--snr', type=int, nargs='+', default=[1, 5, 4], help='Array of snr values.')
+    parser.add_argument('--sigma', type=float, default=1.0, help='Sigma value.')
+    parser.add_argument('--seed', type=int, default=1, help='Random seed.')
+    
+    args = parser.parse_args()
+    
+    μ_array = np.array(args.mu)
+    λ_array = np.array(args.Lambda)
+    n1, n2 = args.n1, args.n2
     γ = generate_symlog_points(n1, n2, 0.1, 10, 1)
-    # γ = np.array([0.1])
-    n_array = np.array([200])
+    n_array = np.array(args.n_array)
     p_array = np.unique((γ * n_array).astype(int))
-    snr_array = np.linspace(1, 5, 4)
-    σ = 1.0
-    seed = 1458
+    snr_array = np.linspace(args.snr[0], args.snr[1], args.snr[2])
+    σ = args.sigma
+    seed = args.seed
+
+    print('Running Simulations')
+    print(f'μ_array: {μ_array}')
+    print(f'λ_array: {λ_array}')
+    print(f'n_array: {n_array}')
+    print(f'p_array: {p_array}')
+    print(f'snr_array: {snr_array}')
+    print(f'σ: {σ}')
+    print(f'seed: {seed}')
 
     start_time = time.time()
     now = datetime.now()
