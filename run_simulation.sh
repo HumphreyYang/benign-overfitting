af8f2a51d2d2fc2aa13fe847dbfc5eb138c9c3fc
Humphrey Yang <humphrey.yang@anu.edu.au>
2023-10-19 16:07:42 +1100

    update shell file

--- run_simulation.sh
+++ run_simulation.sh
@@ -4,11 +4,16 @@ python simulation/lambda_mu_simulations.py --mu 1 3 5 10 50 100 200 500 --Lambda
 python simulation/lambda_mu_simulations.py --mu 1 --Lambda 1 3 5 10 50 100 200 500 --n1 30 --n2 30 --n_array 200 --snr 1 5 4 --sigma 1.0 --test_n 10000 --seed 1858
 
 python simulation/lambda_mu_simulations.py --mu 1 3 5 10 50 100 200 500 --Lambda 1 --n1 30 --n2 30 --n_array 200 --snr 1 5 4 --sigma 1.0 --test_n 10000 --activation abs --seed 2025
-python simulation/lambda_mu_simulations.py --mu 1 3 5 10 50 100 200 500 --Lambda 1 --n1 30 --n2 30 --n_array 200 --snr 1 5 4 --sigma 1.0 --test_n 10000 --activation ReLU --seed 2025
-python simulation/lambda_mu_simulations.py --mu 1 3 5 10 50 100 200 500 --Lambda 1 --n1 30 --n2 30 --n_array 200 --snr 1 5 4 --sigma 1.0 --test_n 10000 --activation tanh --seed 2025
-python simulation/lambda_mu_simulations.py --mu 1 3 5 10 50 100 200 500 --Lambda 1 --n1 30 --n2 30 --n_array 200 --snr 1 5 4 --sigma 1.0 --test_n 10000 --activation gaussian --seed 2025
-
 python simulation/lambda_mu_simulations.py --mu 1 --Lambda 1 3 5 10 50 100 200 500 --n1 30 --n2 30 --n_array 200 --snr 1 5 4 --sigma 1.0 --test_n 10000 --activation abs --seed 2025
-python simulation/lambda_mu_simulations.py --mu 1 --Lambda 1 3 5 10 50 100 200 500 --n1 30 --n2 30 --n_array 200 --snr 1 5 4 --sigma 1.0 --test_n 10000 --activation ReLU --seed 2025
-python simulation/lambda_mu_simulations.py --mu 1 --Lambda 1 3 5 10 50 100 200 500 --n1 30 --n2 30 --n_array 200 --snr 1 5 4 --sigma 1.0 --test_n 10000 --activation tanh --seed 2025
-python simulation/lambda_mu_simulations.py --mu 1 --Lambda 1 3 5 10 50 100 200 500 --n1 30 --n2 30 --n_array 200 --snr 1 5 4 --sigma 1.0 --test_n 10000 --activation gaussian --seed 2025
+
+python simulation/lambda_mu_simulations.py --mu 1 3 5 10 50 100 200 500 --Lambda 1 --n1 30 --n2 30 --n_array 200 --snr 1 5 4 --sigma 1.0 --test_n 10000 --activation quad --seed 2025
+python simulation/lambda_mu_simulations.py --mu 1 --Lambda 1 3 5 10 50 100 200 500 --n1 30 --n2 30 --n_array 200 --snr 1 5 4 --sigma 1.0 --test_n 10000 --activation quad --seed 2025
+
+# python simulation/lambda_mu_simulations.py --mu 1 3 5 10 50 100 200 500 --Lambda 1 --n1 30 --n2 30 --n_array 200 --snr 1 5 4 --sigma 1.0 --test_n 10000 --activation ReLU --seed 2025
+# python simulation/lambda_mu_simulations.py --mu 1 3 5 10 50 100 200 500 --Lambda 1 --n1 30 --n2 30 --n_array 200 --snr 1 5 4 --sigma 1.0 --test_n 10000 --activation tanh --seed 2025
+# python simulation/lambda_mu_simulations.py --mu 1 3 5 10 50 100 200 500 --Lambda 1 --n1 30 --n2 30 --n_array 200 --snr 1 5 4 --sigma 1.0 --test_n 10000 --activation gaussian --seed 2025
+
+# python simulation/lambda_mu_simulations.py --mu 1 --Lambda 1 3 5 10 50 100 200 500 --n1 30 --n2 30 --n_array 200 --snr 1 5 4 --sigma 1.0 --test_n 10000 --activation abs --seed 2025
+# python simulation/lambda_mu_simulations.py --mu 1 --Lambda 1 3 5 10 50 100 200 500 --n1 30 --n2 30 --n_array 200 --snr 1 5 4 --sigma 1.0 --test_n 10000 --activation ReLU --seed 2025
+# python simulation/lambda_mu_simulations.py --mu 1 --Lambda 1 3 5 10 50 100 200 500 --n1 30 --n2 30 --n_array 200 --snr 1 5 4 --sigma 1.0 --test_n 10000 --activation tanh --seed 2025
+# python simulation/lambda_mu_simulations.py --mu 1 --Lambda 1 3 5 10 50 100 200 500 --n1 30 --n2 30 --n_array 200 --snr 1 5 4 --sigma 1.0 --test_n 10000 --activation gaussian --seed 2025
